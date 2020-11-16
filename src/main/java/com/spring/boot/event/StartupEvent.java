649c1502da4a71d49501d8ce8fc65d98781d65bd
Jah <moskovienkor@gmail.com>
2020-11-16 16:41:42 +0200

    sb-01: - travis configured;

--- src/main/java/com/spring/boot/event/StartupEvent.java
+++ src/main/java/com/spring/boot/event/StartupEvent.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -28,8 +27,8 @@ public class StartupEvent implements ApplicationListener<ApplicationReadyEvent> 
 
     public boolean loadCsvFile(String url, String intoPath) {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(intoPath)) {
-            byte dataBuffer[] = new byte[1024];
+                FileOutputStream fileOutputStream = new FileOutputStream(intoPath)) {
+            byte[] dataBuffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
